f3334154d3a50aa06d57c8691b008ee390c422ee
qwertyuiop232 <vk07275@gmail.com>
2017-10-08 16:16:55 -0400

    added a class to understand frames

---
+++ src/animation/GettingStarted.java
@@ -0,0 +1,30 @@
+package animation;
+import java.awt.*;
+
+
+
+public class GettingStarted extends Frame{
+	private static final long serialVersionUID = 1L;
+	
+	public static void main(String [] args) {
+		GettingStarted frame = new GettingStarted();
+		frame.setTitle("My AWT Frame");
+		frame.setSize(600, 600);
+		frame.setLayout(new FlowLayout());
+		//add components		
+		
+		Label label = new Label("Name");
+		frame.add(label);
+		
+		TextField textfield = new TextField();
+		frame.add(textfield);
+		
+		Button button = new Button();
+		frame.add(button);
+		
+		frame.setVisible(true);
+		
+		
+	}
+
+}
