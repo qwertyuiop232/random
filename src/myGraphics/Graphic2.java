caceb4ee6357532c691df116f953d76d18aa50af
qwertyuiop232 <vk07275@gmail.com>
2017-10-10 16:49:20 -0400

    example

---
+++ src/myGraphics/Graphic2.java
@@ -0,0 +1,48 @@
+package myGraphics;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import javax.swing.*;
+
+@SuppressWarnings("serial")
+public class Graphic2 extends JPanel {
+   private static final int RECT_X = 20;
+   private static final int RECT_Y = RECT_X;
+   private static final int RECT_WIDTH = 100;
+   private static final int RECT_HEIGHT = RECT_WIDTH;
+
+   @Override
+   protected void paintComponent(Graphics g) {
+      super.paintComponent(g);
+      // draw the rectangle here
+      g.drawRect(RECT_X, RECT_Y, RECT_WIDTH, RECT_HEIGHT);
+      g.fillRect(RECT_X, RECT_Y, RECT_WIDTH, RECT_HEIGHT);
+      
+   }
+
+   @Override
+   public Dimension getPreferredSize() {
+      // so that our GUI is big enough
+      return new Dimension(RECT_WIDTH + 2 * RECT_X, RECT_HEIGHT + 2 * RECT_Y);
+   }
+
+   // create the GUI explicitly on the Swing event thread
+   private static void createAndShowGui() {
+	  Graphic2 mainPanel = new Graphic2();
+
+      JFrame frame = new JFrame("DrawRect");
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.getContentPane().add(mainPanel);
+      frame.pack();
+      frame.setLocationByPlatform(true);
+      frame.setVisible(true);
+   }
+
+   public static void main(String[] args) {
+      SwingUtilities.invokeLater(new Runnable() {
+         public void run() {
+            createAndShowGui();
+         }
+      });
+   }
+}
